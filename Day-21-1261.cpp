46e648f2efdd42999b89e40af6509ddf3678c572
2018331109 <ahmadulsh@gmail.com>
2025-02-21 12:30:55 +0600

    DFS

---
+++ Day-21-1261.cpp
@@ -0,0 +1,59 @@
+/*
+1261. Find Elements in a Contaminated Binary Tree
+Given a binary tree with the following rules:
+
+    root.val == 0
+    For any treeNode:
+        If treeNode.val has a value x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
+        If treeNode.val has a value x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
+
+Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
+
+Implement the FindElements class:
+
+    FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
+    bool find(int target) Returns true if the target value exists in the recovered binary tree.
+
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    unordered_set<int> st;
+
+    void dfs(TreeNode* root, int x) {
+        if(!root) return;
+
+        root->val=x;
+        st.insert(x);
+
+        dfs(root->left, 2*x+1);
+        dfs(root->right, 2*x+2);
+    }
+
+    FindElements(TreeNode* root) {
+        st.clear();
+        
+        dfs(root, 0);
+        
+    }
+    
+    bool find(int target) {
+        return (st.find(target)!=st.end());
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
